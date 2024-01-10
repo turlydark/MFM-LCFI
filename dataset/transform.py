58021a3e305b36a5d6ebe5726b38b8738fe1a390
Liu Shuai <18265180589@163.com>
2024-01-10 20:40:00 +0800

    Add files via upload

---
+++ dataset/transform.py
@@ -0,0 +1,42 @@
+"""
+
+Author: Andreas Rössler
+"""
+from torchvision import transforms
+
+xception_default_data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5] * 3, [0.5] * 3)
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5] * 3, [0.5] * 3)
+    ]),
+}
+
+two_stream_default_data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*3, [0.5]*3)
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5] * 3, [0.5] * 3)
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5] * 3, [0.5] * 3)
+    ]),
+}
+
